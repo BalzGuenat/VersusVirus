f7dcde48049a3522e6e9a35e0381b15d6f550e54
Kathi Ceesay-Seitz <katharina.c.seitz@gmail.com>
2020-04-04 17:12:31 +0200

    Add first map

--- frontend/index.js
+++ frontend/index.js
@@ -14,3 +14,27 @@ var app = new Vue({
       }
     }
 });
+
+var mymap = L.map('mapid').setView([47.3753731,8.5357199], 15);
+
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+	maxZoom: 18,
+	attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+		'<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+		'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+	id: 'mapbox/streets-v11', 
+	<!-- id: 'mapbox/satellite-v9', -->
+	tileSize: 512,
+	zoomOffset: -1
+}).addTo(mymap); 
+
+
+var circle = L.circle([47.3753682,8.5392164], {
+<!-- var circle = L.circle([47.3768401,8.5406645], { -->
+color: 'red',
+fillColor: '#f03',
+fillOpacity: 0.5,
+radius: 20
+}).addTo(mymap);
+
+circle.bindPopup("230 people");
