a7e1dad152ea7b73e5b91488c7d2553c85e80274
mhoerold <mhoerold@student.ethz.ch>
2020-04-04 22:52:07 +0200

    vue dashbaord

---
+++ vue-argon-dashboard/src/views/Maps.vue
@@ -0,0 +1,107 @@
+<template>
+    <div>
+
+        <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+            <!-- Card stats -->
+            <div class="row">
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Total traffic"
+                                type="gradient-red"
+                                sub-title="350,897"
+                                icon="ni ni-active-40"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Total traffic"
+                                type="gradient-orange"
+                                sub-title="2,356"
+                                icon="ni ni-chart-pie-35"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 12.18%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Sales"
+                                type="gradient-green"
+                                sub-title="924"
+                                icon="ni ni-money-coins"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-danger mr-2"><i class="fa fa-arrow-down"></i> 5.72%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Performance"
+                                type="gradient-info"
+                                sub-title="49,65%"
+                                icon="ni ni-chart-bar-32"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 54.8%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+                </div>
+            </div>
+        </base-header>
+
+        <div class="container-fluid mt--7">
+            <div class="row">
+                <div class="col">
+                    <div class="card shadow border-0">
+                        <div id="map-canvas" class="map-canvas" data-lat="40.748817" data-lng="-73.985428" style="height: 600px;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+  export default {
+    mounted() {
+      var mymap = L.map('map-canvas').setView([47.36667, 8.55], 13);
+
+        L.tileLayer(
+            'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+                attribution: 'Attribution',
+                maxZoom: 17,
+                minZoom: 9
+            }
+        ).addTo(mymap);
+
+        mymap.setZoom(14);
+        var locations = [
+            [47.36667, 8.55, 1],
+            [47.377560, 8.5393, 0.7],
+            [47.377836, 8.5402, 0.5],
+            [47.378389, 8.539804, 1],
+            [47.378258, 8.540040, 1],
+            [47.376699200000004, 8.5328768, 0.2]
+        ];
+
+        var heat = L.heatLayer(locations, { radius: 15 });
+        heat.addTo(mymap);
+    }
+  }
+</script>
+<style>
+</style>
